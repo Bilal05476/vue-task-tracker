cfc6bac44af436c8bb77422f057e91902843ca21
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-07-04 21:14:25 +0500

    updates

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,9 +1,18 @@
 <template>
-  <form @submit="onSubmit" action="">
-    <input type="text" v-model="text" />
-    <input type="text" v-model="day" />
-    <input type="checkbox" v-model="reminder" />
-    <button>Save Task</button>
+  <form @submit="onSubmit">
+    <div class="form-control">
+      <label>Task Name</label>
+      <input v-model="text" type="text" placeholder="Task Name" />
+    </div>
+    <div class="form-control">
+      <label>Task Date</label>
+      <input v-model="day" type="text" placeholder="Task date" />
+    </div>
+    <div class="form-control remainder">
+      <label>Set Reminder</label>
+      <input v-model="reminder" type="checkbox" />
+    </div>
+    <input value="Save" type="submit" class="formBtn" />
   </form>
 </template>
 
@@ -22,7 +31,10 @@ export default {
       e.preventDefault();
       if (!this.text) {
         alert("Please add task");
-      } else if (this.text) {
+      }
+      if (!this.day) {
+        alert("Please add day/date");
+      } else {
         const newTask = {
           id: Math.floor(Math.random() * 100000),
           text: this.text,
@@ -38,3 +50,44 @@ export default {
   },
 };
 </script>
+
+<style>
+form {
+  margin: 10px auto;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  padding: 10px 20px;
+}
+.form-control {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+.form-control input {
+  margin-top: 5px;
+  margin-bottom: 10px;
+  padding: 10px;
+  outline: none;
+  border-radius: 5px;
+}
+.form-control.remainder {
+  align-items: center;
+  flex-direction: row;
+}
+.remainder input {
+  margin-top: 0;
+  margin-bottom: 0;
+  padding: 0;
+}
+.formBtn {
+  display: block;
+  margin: 10px auto;
+  width: 100%;
+  padding: 7px;
+  background-color: #000;
+  border: none;
+  color: #fff;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
