f6471a18b1e136313e9bfd7c0d474464a6c7e3ad
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-07-04 19:36:55 +0500

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-<div class="container">
-  <Header title="Task Trackrer"/>
-</div>
+  <div class="container">
+    <Header title="Task Trackrer" />
+    <Tasks :tasks="tasks" />
+  </div>
 </template>
 
 <script>
-import Header from "./components/Header";
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
 export default {
   name: "App",
-  components:{
-    Header
-  }
+  components: {
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctor Appointment",
+        day: "March 1st at 2:30pm",
+        reminder: false,
+      },
+      {
+        id: 2,
+        text: "Train Station",
+        day: "March 3st at 10:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Client appointment",
+        day: "March 2st at 8:30am",
+        reminder: true,
+      },
+    ];
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  outline: none !important;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="task">
+    <div class="taskText">
+      <h3>{{ task.text }}</h3>
+      <p>{{ task.day }}</p>
+    </div>
+    <i class="fas fa-times"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<style scope>
+.task {
+  background: #f8f8f8;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+</style>
